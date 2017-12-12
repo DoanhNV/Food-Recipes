fbb6cc485bb02c28735012d9e3d8d93d7ee65f81
DESKTOP-859S0FI\admin <springboot>
2017-12-12 17:51:46 +0700

    20171212_DoanhNV: lack of file Step.java

---
+++ FoodRecipes/src/main/java/com/nganle/entity/Step.java
@@ -0,0 +1,59 @@
+package com.nganle.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Step {
+	private String text;
+	private String filePath;
+
+	public Step() {
+
+	}
+
+	public Step(String text, String filePath) {
+		this.text = text;
+		this.filePath = filePath;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	
+	public static List<Step> toListDTO(List<String> texts,List<String> filePaths){
+		List<Step> result = new ArrayList<Step>();
+		for (int i = 0; i < texts.size(); i++) {
+			result.add(new Step(texts.get(i), filePaths.get(i)));
+		}
+		return result;
+	}
+	
+	public static String toSQLArray(List<String> texts,List<String> filePaths){
+		String result = "";
+		int stepNumber = texts.size();
+		for (int i = 0; i < stepNumber; i++) {
+			String stepStr = "{";
+			stepStr += texts.get(i) + "||";
+			stepStr += filePaths.get(i);
+			stepStr += "}";
+			result += stepStr;
+			if( i != (stepNumber -1)){
+				result += ",";
+			}
+		}
+		return result;
+	}
+}
